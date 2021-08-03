7f5f3df2ef79216e5ae3ea6ba2e1825930efd763
David promontor farias <davidgaston2341@gmail.com>
2021-08-02 19:44:56 -0500

    Se crearon los tres archivos JavaScript

---
+++ js/app.js
@@ -0,0 +1,104 @@
+//Creamos un ife para que lo que creemos aqui no se tope con lo demas
+(function(){
+    let DB;
+    document.addEventListener('DOMContentLoaded', ()=>{
+
+       crearDB(); 
+
+       //Este codigo se ejecutara si existe la base de datos indexedDB
+
+       if(window.indexedDB.open('crm', 1)){
+           insertarCliente();
+       }
+    });
+
+    //Crea la base de datos d eindexedDB
+    function crearDB(){
+        const crearDB = window.indexedDB.open('crm', 1);
+
+        crearDB.onerror = function(){
+            console.log('Hubo un error');
+        }
+
+        //En caso de que si se cree la base de datos
+        crearDB.onsuccess = function(){
+            DB = crearDB.result;
+
+        }
+        //Creamos la base de datos
+        crearDB.onupgradeneeded = function(e){
+            const db = e.target.result;
+
+            //le decimos su estructura y le pasamos el id para identificarlo por su id
+            const objectStore = db.createObjectStore('crm', {keypath: 'id', autoIncrement: 'true'});
+
+
+            //creamos los campos con el nombre del campo, el nombre del keypath y si sera unico
+            objectStore.createIndex('nombre', 'nombre', {unique: false});
+            objectStore.createIndex('email', 'email', {unique: true});
+            objectStore.createIndex('telefono', 'telefono', {unique: false});
+            objectStore.createIndex('nombre_empresa', 'nombre_empresa', {unique: false});
+            objectStore.createIndex('id', 'id', {unique: true});
+
+            console.log('Base creada exitosamente');
+        }
+    }
+
+    function insertarCliente(){
+        //Abrimos la conexion
+
+        const abrirConexion = window.indexedDB.open('crm', 1);
+
+        abrirConexion.onerror = function(){
+            console.log('Hubo un error');
+        }
+        abrirConexion.onsuccess = function(){
+            //Obtemeos el resultado de la conexion
+            DB = abrirConexion.result;
+
+            //creamos el objectStore
+            const objectStore = DB.transaction('crm').objectStore('crm');
+            
+            //Utilizamos cursores para iterar sobre los resultados
+            objectStore.openCursor().onsuccess = function(e){
+                //Instanciamos los valores
+                const cursor = e.target.result;
+
+                //Si hay valores se ejecutara lo sifuiente
+                if(cursor){
+                    //Extraemos los valores
+                    const {nombre, email, telefono, empresa, id} = cursor.value;
+                    
+                    const divInformacion = document.querySelector('.body-informacion');
+                    divInformacion.classList.add('informacion-cliente');
+                    divInformacion.innerHTML += `
+                    <tr>
+                        <td>
+                            <p class="campos nombre">${nombre}</p>
+                            <p class="campos email">${email}</p>
+                        </td>
+                        <td>
+                            <p class="campos telefono">${telefono}</p>
+                        </td>
+                        <td>
+                            <p class="campos empresa">${empresa}</p>
+                        </td>
+                        <td>
+                            <a class="btn btn-editar" href="/editarCliente.html">Editar</a>
+                            <a class="btn btn-eliminar" data-cliente="${id}">Eliminar</a>
+                        </td>
+                    
+                    </tr>
+                    `
+
+                    cursor.continue();
+                }
+                else{
+                    console.log('No hay mas resultados')
+                }
+            }
+
+        }
+
+    }
+})();
---
+++ js/nuevoCliente.js
@@ -0,0 +1,117 @@
+(function(){
+    let DB;
+    //Creamos un selector para el formulario
+    const formulario = document.querySelector('.formulario');
+
+    document.addEventListener('DOMContentLoaded', ()=>{
+
+        //Nos conectamos a la base de datos que creamos en app.js
+        conectarDB();
+
+        formulario.addEventListener('submit', validarFormulario);
+    });
+    
+    function conectarDB(){
+        //Nos conectamos a la base de datos
+        const abrirConexion = window.indexedDB.open('crm', 1);
+
+        abrirConexion.onerror = function(){
+            console.log('Hubo un error');
+        }
+        abrirConexion.onsuccess = function(){
+            //Instancia de que todo se conecto bien
+            DB = abrirConexion.result;
+        }
+
+    }
+
+    function validarFormulario(e){
+        e.preventDefault();
+
+        //Creamos selectores locales para cada campo del formulario
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+
+        if(nombre === '' || email === '' || telefono === '' || empresa === ''){
+            
+            imprimirAlerta('Los campos no pueden estar vacios', 'error');
+        }
+
+
+
+        //Creamos un objeto con la informacion obtenida
+
+        const cliente = {
+            nombre, 
+            email,
+            telefono,
+            empresa,
+            id : Date.now()
+        }
+        
+        //Creamos una funcion
+        nuevoCliente(cliente);
+    }
+    function nuevoCliente(cliente){
+        //Nos conectamos a la base de datos
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        
+        //Crear un objeto
+        const objectStore = transaction.objectStore('crm');
+        //Lo añadimos
+        objectStore.add(cliente);
+
+        transaction.onerror = function(){
+            imprimirAlerta('Lo sentimos, el correo ya ha sido registrado', 'error');
+        }
+        transaction.oncomplete = function(){
+            imprimirAlerta('Agregado Correctamente')
+
+            //que nos lleve a la pagina principal al agregarlo correctamente
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
+    }
+
+
+
+
+
+
+
+    function imprimirAlerta(alerta, tipo){
+        const mensaje = document.createElement('DIV');
+        mensaje.textContent = alerta;
+
+        const advertencia = document.querySelector('.alerta');
+
+        if(!advertencia){
+            mensaje.classList.add('alerta');
+
+            if( tipo === 'error'){
+                mensaje.classList.add('mensaje-error');
+            }
+            else{
+                mensaje.classList.add('mensaje-correcto');
+            }
+
+            formulario.insertBefore(mensaje, document.querySelector('.boton-agregar'));
+
+        
+            setTimeout(() => {
+                mensaje.remove();
+            }, 2000);
+
+        }
+
+
+
+
+    }
+
+})();
